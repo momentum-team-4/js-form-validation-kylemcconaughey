eac7520f84b1e51ac61975490dad8e8df044ad4f
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-08-23 14:04:58 -0400

    step 4: hitting submit prints #days*5 in #total iff all fields are filled out correctly

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Add validation!');
+const total = document.querySelector('#total');
+const form = document.querySelector('#parking-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let days = document.querySelector('#days').value;
+    console.log('$' + days + '||' + days * 5);
+    let cost = days * 5;
+
+    total.innerHTML = '$' + cost;
+})
